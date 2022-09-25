a3ac2eb025ff1db67f6a8d672467711880eb5250
Fares Qaddoura <frqd92@gmail.com>
2022-09-26 00:51:45 +0100

    refactor code a little

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 let inputBox = document.getElementById("inputBox");
 let allBtns = document.querySelectorAll(".button");
 let resultsBox = document.getElementById("resultsBox");
-let currentOperator = 0;
 let previousVal = "";
+let currentOperator="";
+let onOff = false;
 let decMode = 0;
 btnStuff();
 function btnStuff(){
@@ -14,46 +15,45 @@ function btnStuff(){
                 case "zero":     
                     if(inputBox.innerText===""){
                     inputBox.innerText="";
-                }   else{inputBox.innerText+=0}; break;
-                case "one":     inputBox.innerText+=1; break;
-                case "two":     inputBox.innerText+=2; break;
-                case "three":   inputBox.innerText+=3; break;
-                case "four":    inputBox.innerText+=4; break;
-                case "five":    inputBox.innerText+=5; break;
-                case "six":     inputBox.innerText+=6; break;
-                case "seven":   inputBox.innerText+=7; break;
-                case "eight":   inputBox.innerText+=8; break;
-                case "nine":    inputBox.innerText+=9; break;
+                }   else{inputBox.innerText+=0}; 
+                displayText();
+                break;
                 case "AC":      aC(); break;
                 case "C":       erase(); break;
                 case "equals": break;
-                case "multiply": break;
+                case "plus":  break;
+                case "multiply":  break;
                 case "decimal": 
                     if(decMode===0){
                         if(inputBox.innerText===""){
-                            inputBox.innerText="0.";
+                        inputBox.innerText="0.";
                         }   else{inputBox.innerText+="."}; decMode=1;
                     }
-                    else{
-
-                    }
+                    displayText();
                     break;
+                default: 
+                inputBox.innerText+=e.target.innerText;
+                displayText();
             }
         })
     }
 }
 
+function displayText(){
+    resultsBox.innerText=inputBox.innerText;
+}
 
 function erase(){
-    let length = inputBox.innerText.length -1;
+
     if(inputBox.innerText[length]==="."){
         decMode=0;
     }
-    inputBox.innerText= inputBox.innerText.slice(0, length)
-    
+    inputBox.innerText= inputBox.innerText.slice(0, -1)
+    displayText();
 }
 function aC(){
     inputBox.innerText=""; 
+    resultsBox.innerText="";
     previousVal=0;
     currentOperator = 0;
     decMode=0;
