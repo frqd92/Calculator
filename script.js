8655afb8d1a4fa9d3e348e9dacae5ce7c11b471d
Fares Qaddoura <frqd92@gmail.com>
2022-09-26 14:07:40 +0100

    fix some issues with decimal point

--- script.js
+++ script.js
@@ -5,52 +5,60 @@ let allBtns = document.querySelectorAll(".button");
 let resultsBox = document.getElementById("resultsBox");
 let previousVal = "";
 let currentOperator="";
-let onOff = false;
+let onOff = 0;
 let decMode = 0;
 btnStuff();
 function btnStuff(){
     for(let i=0;i<allBtns.length;i++){
         allBtns[i].addEventListener("click", (e)=>{
+            console.log(decMode);
+            if(inputBox.textContent && onOff==1){
+                inputBox.textContent="";
+                onOff=0;
+            }
             switch(e.target.classList[2]){
                 case "zero":     
                     if(inputBox.innerText===""){
                     inputBox.innerText="";
                 }   else{inputBox.innerText+=0}; 
-                displayText();
                 break;
                 case "AC":      aC(); break;
                 case "C":       erase(); break;
-                case "equals": break;
+                case "equals": equals(); break;
                 case "plus":  break;
-                case "multiply":  break;
+                case "multiply": equals("*"), onOff=1; break;
                 case "decimal": 
                     if(decMode===0){
                         if(inputBox.innerText===""){
                         inputBox.innerText="0.";
                         }   else{inputBox.innerText+="."}; decMode=1;
                     }
-                    displayText();
-                    break;
-                default: 
+                break;
+                default:
                 inputBox.innerText+=e.target.innerText;
-                displayText();
+               
             }
         })
     }
 }
-
-function displayText(){
-    resultsBox.innerText=inputBox.innerText;
+function equals(input){
+    previousVal=parseFloat(inputBox.innerText);
+    console.log(previousVal);
+    if(!input){
+        inputBox.textContent = previousVal * inputBox.textContent;
+    }
 }
 
-function erase(){
 
-    if(inputBox.innerText[length]==="."){
+function erase(){
+    let length = inputBox.innerText.length
+    if(inputBox.innerText[length-1]==="."){
         decMode=0;
     }
     inputBox.innerText= inputBox.innerText.slice(0, -1)
-    displayText();
+ 
 }
+
 function aC(){
     inputBox.innerText=""; 
     resultsBox.innerText="";
