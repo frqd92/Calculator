4a7406943c681c5019b4e59dbe123640d860773d
Fares Qaddoura <frqd92@gmail.com>
2022-09-27 23:32:14 +0100

    fix minor bugs

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let previousVal = "", currentOperator="", currentBtn ="";
 let onOff = 0, decMode = 0;
 
 window.onload = function(){
+    keyboard();
     for(let i=0;i<allBtns.length;i++){
         allBtns[i].addEventListener("click", (e)=>{
+            console.log("onOff: "+  onOff);
+            console.log("decMode: "+  onOff);
             if(inputBox.textContent && onOff==1 && e.target.classList[3]==="num"){
                 inputBox.textContent="";
                 onOff=0;
@@ -16,13 +19,13 @@ window.onload = function(){
             switch(e.target.classList[2]){
                 case "zero":     
                 if(currentOperator==="/"){console.log("hello")} //don't divide by 0
-                    if(inputBox.innerText===""){
-                    inputBox.innerText="";
+                    if(inputBox.innerText==="" && onOff=== 0){
+                    inputBox.innerText=""; decMode=0;
                 }   else{inputBox.innerText+=0}; 
                 break;
                 case "AC": aC(); break;
                 case "C": erase(); break;
-                case "equals": equals(); break;
+                case "equals":  equals(); break;
                 case "plus": operator("+"); break;
                 case "multiply": operator("*"); break;
                 case "subt": operator("-"); break;
@@ -44,16 +47,61 @@ window.onload = function(){
         })
     }
 }
+function keyboard(){
+    document.addEventListener("keydown", e=>{
+        console.log("onOff: "+  onOff);
+        console.log("decMode: "+  onOff);
+        let regex = (/^[1-9]+$/.test(e.key));
+        if(inputBox.textContent && onOff==1 && regex){
+            inputBox.textContent="";
+            onOff=0;
+        }
+        if(regex){
+          inputBox.innerText+=e.key;
+        }
+
+        switch(e.key){
+            case "0":                     
+                if(inputBox.innerText===""){
+                inputBox.innerText="";
+            }   else{inputBox.innerText+=0}; break;
+            case "+": operator("+"); break;
+            case "*": operator("*"); break;
+            case "/": operator("/"); break;
+            case "-": operator("-"); break;
+            case  ".":     
+                if(decMode===0){
+                if(inputBox.innerText===""){
+                inputBox.innerText="0.";
+                }   else{inputBox.innerText+="."}; decMode=1; break;
+            } break;
+            case "Enter": equals(); break;
+            case "Backspace": erase(); break;
+            case "Escape": aC(); break;
+        }
+
+    })
+}
+
 function equals(){
+    decMode=0;
+    if(onOff===0){
         switch(currentOperator){
             case "*":inputBox.textContent= previousVal * inputBox.textContent; break;
-            case "+": inputBox.textContent= previousVal + parseFloat(inputBox.textContent); break;
+            case "+": inputBox.textContent= parseFloat(previousVal) + parseFloat(inputBox.textContent); break;
             case "-": inputBox.textContent= previousVal - parseFloat(inputBox.textContent); break;
             case "/": inputBox.textContent= previousVal / parseFloat(inputBox.textContent); break;
             case "^": inputBox.textContent = previousVal ** parseFloat(inputBox.textContent); break; 
         }
+        onOff=1;
+    }
         previousVal=inputBox.textContent;
         resultsBox.innerText=inputBox.innerText;
+      
+        currentOperator="";
+    console.log(inputBox.textContent.includes("."))
+           
+
 }
 function operator(input){
         onOff=1; decMode=0;
@@ -61,10 +109,11 @@ function operator(input){
             currentOperator=input;
             previousVal=parseFloat(inputBox.innerText);
         }
+
     upperBox();
 }
 function upperBox(x){
-    if(x){resultsBox.innerText=inputBox.innerText;} //for sqrt display
+    if(x){resultsBox.innerText=inputBox.innerText;}
     else{
         if(inputBox.innerText){
             let arr = [];
